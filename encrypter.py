e86642f38ba28cb89e5998cae2bb523cf77f6a2e
TheKrispOfTheGits <christian.choe@student.education.wa.edu.au>
2020-05-07 16:40:09 +0800

    Version: 0.2

--- encrypter.py
+++ encrypter.py
@@ -1,10 +1,17 @@
 import time
 import random
 
+print()
+print()
 print("Hello")
 time.sleep(0.5)
 print("I am an encrypter/decrypter.")
 time.sleep(0.5)
-keytrue = input("Do you have a key?")
-if keytrue == "Yes":
-    print("Yes")
+enode = input("Are you encrypting or decrypting? ")
+enode = enode.upper()
+if enode == "ENCRYPTING":
+    pubkeytrue = input("Do you have a lock? ")
+    pubkeytrue = pubkeytrue.upper()
+    if pubkeytrue == "YES":
+        pubkey = input("What is your lock? ")
+
