989a9f619441b1b3193d367e817f18f0aae1db54
TheKrispOfTheGits <christian.choe@student.education.wa.edu.au>
2020-05-07 12:34:40 +0800

    First Commit [Rev]

---
+++ encrypter.py
@@ -0,0 +1,13 @@
+import time
+import random
+
+print("Hello")
+time.sleep(0.5)
+print("I am an encrypter/decrypter.")
+time.sleep(0.5)
+keytrue = input("Do you have a key?")
+if keytrue == "Yes":
+
+
+
+
