db54ccf2c1bd72a55c63404bc3134110d2038624
TheKrispOfTheGits <christian.choe@student.education.wa.edu.au>
2020-05-07 16:59:03 +0800

    Version: 0.2.1

--- encrypter.py
+++ encrypter.py
@@ -1,17 +1,34 @@
 import time
 import random
+from random import seed
+from random import randint
 
-print()
-print()
+seed(1)
 print("Hello")
 time.sleep(0.5)
 print("I am an encrypter/decrypter.")
 time.sleep(0.5)
 enode = input("Are you encrypting or decrypting? ")
 enode = enode.upper()
+
 if enode == "ENCRYPTING":
     pubkeytrue = input("Do you have a lock? ")
     pubkeytrue = pubkeytrue.upper()
+
     if pubkeytrue == "YES":
         pubkey = input("What is your lock? ")
 
+    elif pubkeytrue == "NO":
+        pubkeyverif = input("Do you want to create a lock? ")
+        pubkeyverif = pubkeyverif.upper()
+        
+        if pubkeyverif == "YES":
+            for aa in range(1):
+                pubkey = randint(100,999)
+            print(f"Your lock is: {pubkey}")
+                
+        
+        
+for aa in range(1):
+    value = randint(0,94)
+    print(value)
